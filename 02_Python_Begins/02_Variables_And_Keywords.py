5be7c1eeb359374b4c933cc1ef0ead103e0d04f4
Prasadaware8125 <prasadaware6613@gmail.com>
2026-01-21 23:49:51 +0530

    Day 1 : Add examples of Variables and Keywords

---
+++ 02_Python_Begins/02_Variables_And_Keywords.py
@@ -0,0 +1,19 @@
+a = 10
+a = "Welcome to the class"
+print(a)
+print(type(a))
+
+my_age = 20
+print("My is age is ",my_age,"and its data type is",type(my_age))
+
+score = 9.5
+print("My is score is ",score,"and its data type is",type(score))
+
+a = True
+print(a,"and its data type is",type(a))
+
+def sum_function(a,b):
+    c = a+b
+    return c
+
+print(sum_function(5,10))
